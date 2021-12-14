92852c3399b22d27b61c90d1f0d3250863f63508
Carr <carr2838@bellsouth.net>
2021-12-13 20:49:20 -0500

    Capstone 1 Product Solution

--- src/main/java/com/techelevator/Product.java
+++ src/main/java/com/techelevator/Product.java
@@ -1,2 +1,40 @@
-package com.techelevator;public class Product {
+package com.techelevator;
+
+public class Product {
+    private String name;
+    private double price;
+    private String typeOfProduct;
+    private String location;
+    private int stock = 5;
+
+    public Product(String name, double price, String typeOfProduct, String location) {
+        this.name = name;
+        this.price = price;
+        this.typeOfProduct = typeOfProduct;
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        if (typeOfProduct.equalsIgnoreCase("Chip")) {
+            return "Crunch Crunch Yum";
+        }
+        else if (typeOfProduct.equalsIgnoreCase("Candy")) {
+            return "Munch Munch Yum"; }
+
+        else if (typeOfProduct.equalsIgnoreCase("Drink")) {
+            return "Glug Glug Yum"; }
+
+        else if (typeOfProduct.equalsIgnoreCase("Gum")) {
+            return "Chew Chew Yum"; }
+
+        else {
+            return "Do Not Have Type";
+        }
+
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
+
