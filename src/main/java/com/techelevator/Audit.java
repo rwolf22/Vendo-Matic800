2fccbdf0e5244fbf539b8f0223c7892231ef2da7
Carr <carr2838@bellsouth.net>
2021-12-16 18:17:42 -0500

    Capstone 1 Audit Solution

---
+++ src/main/java/com/techelevator/Audit.java
@@ -0,0 +1,30 @@
+package com.techelevator;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Audit {
+
+    public static void log(String action, double moneyBefore, double moneyAfter) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter myFormatt = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+        String formattedDate = now.format(myFormatt);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        File logFile = new File("Log.txt");
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(logFile, true))) {
+            writer.println(formattedDate + " " + action + " " + currency.format(moneyBefore) + " " + currency.format(moneyAfter));
+        } catch (IOException e) {
+            System.out.println("File Not Found");
+        }
+    }
+
+    public static void main(String[] args) {
+        log("Test", 17.05, 67.55);
+    }
+
+}
+
